51fbae608023f5b58773409554e20d8e1e986e75
shuvocasanova <shuvocasanova@gmail.com>
2020-04-28 20:47:22 +0600

    my first commit

---
+++ src/components/credit/Credit.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+
+import styles from './Credit.module.css';
+
+
+const Credit = () => {
+    return(
+        <div className={styles.header}>
+            <h1>COVID-19 Status developed by Shuvo Kumer Das</h1>
+        </div>
+    )
+}
+
+export default Credit
